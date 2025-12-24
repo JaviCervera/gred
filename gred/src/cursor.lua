18ee7307a8b08e79551670e590d83cdca74dc76d
JaviCervera <jedive@gmail.com>
2025-12-24 10:57:14 +0100

    Translated Cursor to HAL

--- gred/src/cursor.lua
+++ gred/src/cursor.lua
@@ -1,49 +1,50 @@
 Cursor = class()
+Cursor.COLOR_ORANGE = Color()
+Cursor.COLOR_ORANGE.r = 255
+Cursor.COLOR_ORANGE.g = 161
+Cursor.COLOR_ORANGE.b = 0
+Cursor.COLOR_ORANGE.a = 255
+Cursor.COLOR_WHITE = Color()
+Cursor.COLOR_WHITE.r = 255
+Cursor.COLOR_WHITE.g = 255
+Cursor.COLOR_WHITE.b = 255
+Cursor.COLOR_WHITE.a = 255
 
 function Cursor:Create(grid)
   self = self:New()
-  local mesh = CreateCubeMesh()
-  self.entity = CreateModel(mesh)
+  self.position = Vector3()
   self.grid = grid
   self.alpha = 0.5
   self.alpha_dir = 1
-  FreeMesh(mesh)
-  local mat = EntityMaterial(self.entity, 1)
-  SetMaterialType(mat, MATERIAL_ALPHA)
-  SetMaterialFlag(mat, FLAG_LIGHTING, false)
-  SetMaterialFlag(mat, FLAG_VERTEXCOLORS, true)
   self:reset()
   return self
 end
 
 function Cursor:reset()
-  SetEntityPosition(self.entity, self.grid:tilesX() / 2, self.grid:tilesY() / 2, self.grid:tilesZ() / 2)
+  self.position.x = self.grid:tilesX() / 2
+  self.position.y = self.grid:tilesY() / 2
+  self.position.z = self.grid:tilesZ() / 2
 end
 
 function Cursor:update(editing)
-  if editing and not EntityVisible(self.entity) then SetEntityVisible(self.entity, true) end
-  if not editing and EntityVisible(self.entity) then SetEntityVisible(self.entity, false) end
   if editing then
     -- Update alpha
-    self.alpha = self.alpha + self.alpha_dir * 0.5 * DeltaTime()
+    self.alpha = self.alpha + self.alpha_dir * 0.5 * GetFrameTime()
     if self.alpha <= 0.25 or self.alpha >= 0.75 then
       self.alpha = Clamp(self.alpha, 0.25, 0.75)
       self.alpha_dir = self.alpha_dir * -1
     end
-    SetMeshColor(ModelMesh(self.entity), FadeColor(COLOR_ORANGE, self.alpha * 255))
-    UpdateMesh(ModelMesh(self.entity))
 
     -- Update position
-    if KeyHit(KEY_UP) then TranslateEntity(self.entity, 0, 0, 1) end
-    if KeyHit(KEY_DOWN) then TranslateEntity(self.entity, 0, 0, -1) end
-    if KeyHit(KEY_LEFT) then TranslateEntity(self.entity, -1, 0, 0) end
-    if KeyHit(KEY_RIGHT) then TranslateEntity(self.entity, 1, 0, 0) end
-    if KeyHit(KEY_Q) then TranslateEntity(self.entity, 0, 1, 0) end
-    if KeyHit(KEY_A) then TranslateEntity(self.entity, 0, -1, 0) end
-    SetEntityPosition(
-      self.entity,
-      Clamp(EntityX(self.entity), 1, self.grid:tilesX()),
-      Clamp(EntityY(self.entity), 1, self.grid:tilesY()),
-      Clamp(EntityZ(self.entity), 1, self.grid:tilesZ()))
+    if IsKeyPressed(KEY_UP) then self.position.z = Clamp(self.position.z - 1, 1, self.grid:tilesZ()) end
+    if IsKeyPressed(KEY_DOWN) then self.position.z = Clamp(self.position.z + 1, 1, self.grid:tilesZ()) end
+    if IsKeyPressed(KEY_LEFT) then self.position.x = Clamp(self.position.x - 1, 1, self.grid:tilesX()) end
+    if IsKeyPressed(KEY_RIGHT) then self.position.x = Clamp(self.position.x + 1, 1, self.grid:tilesX()) end
+    if IsKeyPressed(KEY_Q) then self.position.y = Clamp(self.position.y - 1, 1, self.grid:tilesY()) end
+    if IsKeyPressed(KEY_A) then self.position.y = Clamp(self.position.y - 1, 1, self.grid:tilesY()) end
   end
+end
+
+function Cursor:draw()
+  DrawCube(self.position, 1, 1, 1, ColorLerp(Cursor.COLOR_ORANGE, Cursor.COLOR_WHITE, self.alpha))
 end
