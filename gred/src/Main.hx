439313500c2b63cf3dcdf835a92b85b5fbd79bf1
JaviCervera <JaviCervera@users.noreply.github.com>
2026-05-10 12:18:47 +0200

    Removed custom font

--- gred/src/Main.hx
+++ gred/src/Main.hx
@@ -6,8 +6,6 @@ class Main {
 
 		Cs.openScreen(1024, 768, Cs.desktopDepth(), Cs.SCREEN_RESIZABLE | Cs.SCREEN_VSYNC);
 
-		final font = Cs.loadFont("FSEX300.ttf", 16);
-
 		final textureNames = TextureReader.read(TEX_PATH);
 		final ceilingTexViewer = new TextureViewer(textureNames, 1, TEX_PATH, Cs.KEY_W, Cs.KEY_E);
 		final wallTexViewer = new TextureViewer(textureNames, 2, TEX_PATH, Cs.KEY_S, Cs.KEY_D);
@@ -33,13 +31,13 @@ class Main {
 			flagMgr.update(gridMgr.getCurrentFlag());
 
 			Cs.drawWorld();
-			flagMgr.drawFlagNumbers(font, cam.entity);
+			flagMgr.drawFlagNumbers(null, cam.entity);
 			if (gridMgr.isEditing()) {
 				ceilingTexViewer.draw(Cs.screenWidth() - 144, 16, 128, 128);
 				wallTexViewer.draw(Cs.screenWidth() - 144, 160, 128, 128);
 				floorTexViewer.draw(Cs.screenWidth() - 144, 304, 128, 128);
-				Cs.drawText(font, "[F1] New -- [F2] Load -- [F3] Save -- [F4] Mode: " + gridMgr.getModeName() + " -- [ENTER] Preview", 8, 8, Cs.COLOR_WHITE);
-				Cs.drawText(font,
+				Cs.drawText(null, "[F1] New -- [F2] Load -- [F3] Save -- [F4] Mode: " + gridMgr.getModeName() + " -- [ENTER] Preview", 8, 8, Cs.COLOR_WHITE);
+				Cs.drawText(null,
 					"[F] "
 					+ enableDisableText(grid.filteringEnabled())
 					+ " texture filtering -- "
@@ -50,13 +48,13 @@ class Main {
 					+ enableDisableText(grid.wireframeEnabled())
 					+ " wireframe",
 					8, 24, Cs.COLOR_WHITE);
-				Cs.drawText(font, "[U/I] Select flag number (current: "
+				Cs.drawText(null, "[U/I] Select flag number (current: "
 					+ gridMgr.getCurrentFlag()
 					+ ") -- "
 					+ "[P] Place flag -- "
 					+ "[O] Delete flag", 8,
 					40, Cs.COLOR_WHITE);
-				Cs.drawText(font,
+				Cs.drawText(null,
 					"Cursor Position "
 					+ Cs.int(Cs.entityX(cursor.entity))
 					+ "x"
